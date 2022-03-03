a93bcae24e1d6ef85cb48b81792d0dfcc045253b
Mainul Islam <maainul.islam@gmail.com>
2022-03-03 17:29:54 +0600

    Implement add new book using user input

--- index.js
+++ index.js
@@ -11,16 +11,15 @@ function Book (title, author, pages, isRead) {
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
+    console.log("added book");
     let book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
+    console.log("added book next");
+
 
 }
 
-addBookToLibrary("Clean Code", "Mainul", "299", true);
-addBookToLibrary("Refactor Code", "Nirob", "192", false);
-addBookToLibrary("Test Code", "Ridoy", "302", true);
-addBookToLibrary("Test Code", "Ridoy", "302", true);
-addBookToLibrary("Test Code", "Ridoy", "302", true);
+// addBookToLibrary("Clean Code", "Mainul", "299", true);
 
 
 function displayBook() {
@@ -32,21 +31,31 @@ function displayBook() {
         bookContainer.innerHTML = `<h1 class="title">${book.title}</h1>
                                 <h3 class="author">${book.author}</h3>
                                 <h5 class="pages">${book.pages}</h5>
-                                <p className="info">${book.info()}</p>`
+                                <p className="info">${book.info()}</p>
+                                <button id="toggle-read">${book.isRead ? "Read" : "Not Read"}</button>
+                                <button id="remove-book">Remove</button`
         container.appendChild(bookContainer);
     })
 }
 
-displayBook();
 
 //
 let modal = document.getElementById("bookFormModal");
 let addBookBtn = document.getElementById("addBook");
 let modalCloseBtn = document.getElementById("closeBtn")
+let modalForm = document.getElementById("modal-form");
+let toggleRead = document.getElementById("toggle-read");
+let removeBook = document.getElementById("remove-book");
+
 
 addBookBtn.onclick = () => openModal();
 modalCloseBtn.onclick = () => closeModal();
 window.addEventListener("click", outSideClick);
+modalForm.addEventListener("submit", handleAddBook);
+toggleRead.addEventListener("click", () => {
+    
+})
+
 
 function openModal() {
     modal.style.display = "flex";
@@ -61,5 +70,30 @@ function outSideClick(e) {
         modal.style.display = "none";
     }
 }
+
+function handleAddBook(e) {
+    e.preventDefault();
+
+    let title = e.target.title.value;
+    let author = e.target.author.value;
+    let pages = e.target.pages.value;
+    let read = e.target.read.checked;
+
+
+    addBookToLibrary(title, author, pages, read);
+    console.log(myLibrary);
+
+    // title = "";
+    // author = "";
+    // pages = "";
+    // read = "";
+    displayBook();
+
+    closeModal();
+
+}
+
+displayBook();
+
 
 
