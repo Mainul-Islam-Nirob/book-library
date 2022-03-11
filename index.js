4ae415960110c1aa49fce49ac927e56f0721f658
Mainul Islam <maainul.islam@gmail.com>
2022-03-11 14:35:44 +0600

    Refactor: seperate dom related code to bookCard function from displayBooks

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let modal = document.getElementById("bookFormModal");
-let addBookBtn = document.getElementById("addBook");
-let modalCloseBtn = document.getElementById("closeBtn")
-let modalForm = document.getElementById("modal-form");
-let toggleRead = document.getElementById("toggle-read");
-let removeBook = document.getElementById("remove-book");
-let container = document.getElementById("container")
-
+const modal = document.getElementById("bookFormModal");
+const addBookBtn = document.getElementById("addBook");
+const modalCloseBtn = document.getElementById("closeBtn")
+const modalForm = document.getElementById("modal-form");
+const toggleRead = document.getElementById("toggle-read");
+const removeBook = document.getElementById("remove-book");
+const container = document.getElementById("container")
 
 let myLibrary = [];
 
@@ -28,62 +27,69 @@ function addBookToLibrary(title, author, pages, isRead) {
 }
 
 
-function displayBook() {
-    container.innerHTML = ""; //Removing all child elements of container before adding new book
+function bookCard(book) {
+    const bookCard = document.createElement("div");
+    const bookDetail = document.createElement("div");
+    const title = document.createElement("h1");
+    const author = document.createElement("h3");
+    const pages = document.createElement("h5");
+    const btnGroup = document.createElement("div");
+    const readBtn = document.createElement("button");
+    const removeBtn = document.createElement("button");
 
-    myLibrary.map((book) => {
-        const bookCard = document.createElement("div");
-        const bookDetail = document.createElement("div");
-        const title = document.createElement("h1");
-        const author = document.createElement("h3");
-        const pages = document.createElement("h5");
-        const btnGroup = document.createElement("div");
-        const readBtn = document.createElement("button");
-        const removeBtn = document.createElement("button");
+    bookCard.classList.add("book-card");
+    bookDetail.classList.add("book-detail");
+    title.classList.add("title");
+    author.classList.add("author");
+    pages.classList.add("pages");
+    btnGroup.classList.add("btn-group");
+    readBtn.classList.add("btn");
+    removeBtn.classList.add("btn");
 
-        bookCard.classList.add("book-card");
-        bookDetail.classList.add("book-detail");
-        title.classList.add("title");
-        author.classList.add("author");
-        pages.classList.add("pages");
-        btnGroup.classList.add("btn-group");
-        readBtn.classList.add("btn");
-        removeBtn.classList.add("btn");
-
-        readBtn.setAttribute("id", "toggle-read");
-        removeBtn.setAttribute("id", "remove-book");
+    readBtn.setAttribute("id", "toggle-read");
+    removeBtn.setAttribute("id", "remove-book");
 
 
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pages.textContent = `${book.pages} Pages`;
-        removeBtn.textContent = "Remove"
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} Pages`;
+    removeBtn.textContent = "Remove"
 
-        if (book.isRead) {
-            readBtn.textContent = "Read";
-            readBtn.classList.add("read-btn");
-        } else {
-            readBtn.textContent = "Not Read";
-            readBtn.classList.add("not-read-btn");
-        }
-
-
-        readBtn.addEventListener("click", () => handleToggleRead(book.id));
-        removeBtn.addEventListener("click", () => handleRemoveBook(book.id));
-
-        bookDetail.appendChild(title);
-        bookDetail.appendChild(author);
-        bookDetail.appendChild(pages);
-        btnGroup.appendChild(readBtn);
-        btnGroup.appendChild(removeBtn);
-        bookCard.appendChild(bookDetail);
-        bookCard.appendChild(btnGroup);
-
-        container.appendChild(bookCard);
-    })
+    if (book.isRead) {
+        readBtn.textContent = "Read";
+        readBtn.classList.add("read-btn");
+    } else {
+        readBtn.textContent = "Not Read";
+        readBtn.classList.add("not-read-btn");
+    }
 
 
+    readBtn.addEventListener("click", () => handleToggleRead(book.id));
+    removeBtn.addEventListener("click", () => handleRemoveBook(book.id));
+
+    bookDetail.appendChild(title);
+    bookDetail.appendChild(author);
+    bookDetail.appendChild(pages);
+    btnGroup.appendChild(readBtn);
+    btnGroup.appendChild(removeBtn);
+    bookCard.appendChild(bookDetail);
+    bookCard.appendChild(btnGroup);
+
+    container.appendChild(bookCard);
 }
+
+
+const resetBooks = () => {
+    container.innerHTML = "";
+}
+
+function displayBook() {
+    resetBooks();
+    myLibrary.map((book) => {
+        bookCard(book);
+    })
+}
+
 
 
 function openModal() {
@@ -116,17 +122,13 @@ function handleAddBook(e) {
 }
 
 function handleToggleRead(id) {
-    console.log("toggle read", id);
     let toggleBook = myLibrary.find(book => book.id == id)
-    console.log(toggleBook, "1");
     toggleBook.toggleRead();
-    console.log(toggleBook, "2");
     displayBook();
     
 }
 
 function handleRemoveBook(id) {
-    console.log("remove book", id);
     myLibrary = myLibrary.filter(book => book.id != id);
     displayBook();
 }
